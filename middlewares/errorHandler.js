80c2c3d7b0e1c95e208b2d02db0c4280a32bf190
hr6h6m <hr6h6m@192.168.1.60>
2026-06-15 16:49:52 +0300

    Add error handler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  console.error("[ERROR LOG]:", err);
+  req.flash("notification", {
+    success: false,
+    msg: `An error has occurred 😔`,
+  });
+  res.redirect("/messages");
+};
+
+module.exports = errorHandler;
